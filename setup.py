90e02d69f49df91b3e8d464b765ad67d65d6cb32
José Vizcaya <josevizcaya0616@gmail.com>
2022-03-19 00:38:39 -0400

    modf: Added some other useful information to the 'setup.py' file.

--- setup.py
+++ setup.py
@@ -4,15 +4,23 @@ from setuptools import setup
 setup(
     name="pykage",
     version="0.1",
+    author="José Vizcaya",
+    author_email="josevizcaya0616@gmail.com",
+    description=
+    """A CLI app provided for creating packages in
+    environments that does not support that functionality,
+    i.e the vast majority of text editors.
+    """,
     packages=[
         "pykage-app"
     ],
-    py_modules=[],
     install_requires=[
         "click",
         "colorama"
     ],
     entry_points={
-        "console_scripts": []
+        "console_scripts": [
+            "pykage = "
+        ]
     }
 )
