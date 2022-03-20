29ed2d1fca5c32003137b38c9e3944d45f662513
José Vizcaya <josevizcaya0616@gmail.com>
2022-03-19 20:54:51 -0400

    modf: Modified and simplified the process of filepaths handling

--- pykage-app/packaging/create_package.py
+++ pykage-app/packaging/create_package.py
@@ -1,10 +1,7 @@
 from datetime import date
-from os import getcwd, mkdir, getenv
-from os.path import (
-                abspath, 
-                relpath, 
-                join,
-            )
+from os import getcwd, getenv
+from os.path import abspath
+from pathlib import Path
 from typing import Optional
 
 
@@ -20,19 +17,24 @@ class CreatePackage:
     def __init__(self, package_name:str, dir:Optional[str]) -> None:
         self.package_name = package_name
         self.created_date = date.today()
-        self.abs_dir = getcwd() or abspath(dir)
-        self.abs_init_dir = ""
+        self.abs_dir = Path(getcwd() or abspath(dir))
+        self.abs_init_dir = None
 
     def create_package(self):
-        new_package_route = join(self.abs_dir, self.package_name)
+        """It creates a handly method to create python
+            packages.
+        """
+
+        # npr stands for "new package route 
+        # (Yeah i'm lazy with long names) don't bother me.
+        npr = self.abs_dir / self.package_name
         
         try:
-            mkdir(new_package_route)
+            npr.mkdir()
         except FileNotFoundError as dir_error:
-            print(f"There's was an error during folder creation:\n\t{dir_error}") 
+            raise FileNotFoundError("There was an error during package creation process")
         
-        self.abs_init_dir = join(abspath(new_package_route), "__init__.py")
-
+        self.abs_init_dir = npr / "__init__.py"
         self._create_init_file(True)
 
     def _create_init_file(self, is_package_created:bool) -> None:
@@ -43,9 +45,10 @@ class CreatePackage:
         template_str = f"""# {self.package_name.upper()} package created at {self.created_date.strftime("%m/%d/%Y")}\n         
         # by {getenv("USER").upper()}.\n
         # \n
-        # PYKAGE is created by VizMan0616 and all it's feature is a software running under the MIT License.
+        # PYKAGE is created by Jos'e Vizcaya. All features is a software running under the MIT License.\n
+        # To see more about please refer to [GitHub link]
         """
 
         if is_package_created:
-            with open(self._rel_init_dir, "w+") as init_file:
-                init_file.write(template_str)
+            with self.abs_init_dir.open(mode="w") as init_file:
+                init_file.write(template_str)    
