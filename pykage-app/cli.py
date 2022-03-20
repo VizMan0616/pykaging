caea695fabf29cd0b28d17345b6c5ab928db7cee
José Vizcaya <josevizcaya0616@gmail.com>
2022-03-19 22:09:01 -0400

    modf: Added CLI logic

--- pykage-app/cli.py
+++ pykage-app/cli.py
@@ -1,11 +1,25 @@
+from os import getcwd
+from .packaging import CreatePackage
+
+
 try:
     import click
     import colorama as color
     from colorama import Fore
 except ImportError as import_error:
-    print(f"There was a error during module import:\n {import_error}")
+    raise ImportError("There was an error during import process.")
+
 
 color.init(autoreset=True)
 
-def main():
-    pass
+@click.command()
+@click.argument("name", type=click.STRING, required=True)
+@click.option("-d", "--dir",
+                is_flag=False,
+                flag_value=getcwd(),
+                default=getcwd(),
+                help="The directory in wich the package should be created.",
+                type=click.Path(exists=True))
+def main(name, dir):
+    new_package = CreatePackage(name, dir)
+    new_package.create_package()
