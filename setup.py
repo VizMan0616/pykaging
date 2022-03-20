455331c46703f97d55014e587bca71070bc5eee9
José Vizcaya <josevizcaya0616@gmail.com>
2022-03-19 23:47:44 -0400

    fix: Some issues iwth how the package work

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
-from setuptools import setup
-
+from setuptools import setup, find_packages
 
 setup(
     name="pykage",
     version="0.1",
     author="José Vizcaya",
     author_email="josevizcaya0616@gmail.com",
-    description=
-    """A CLI app provided for creating packages in
+    description="""A CLI app provided for creating packages in
     environments that does not support that functionality,
     i.e the vast majority of text editors.
     """,
     packages=[
-        "pykage-app"
+        "pykage_app",
+        "pykage_app.packaging"
     ],
     install_requires=[
         "click",
@@ -20,7 +19,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "pykage = "
+            "pykage = pykage_app:main"
         ]
     }
 )
